f815ab316258e51ccde494e14f83019d85f5cbea
AmiR <amirmsvi66@gmail.com>
2024-04-22 01:44:45 -0400

    added sidebar for dashboard and contents

--- src/components/DashboardPage.tsx
+++ src/components/DashboardPage.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import SideBar from './SideBar';
+import Content from './Content';
+
 
 function DashboardPage() {
+    
   return (
-    <div>DashboardPage</div>
+    <div className="flex h-screen"> {/* Flex container for sidebar and content */}
+    <SideBar /> {/* Your sidebar on the left */}
+    
+    <div className="flex-1 p-4"> {/* Main content area */}
+      <Content /> {/* Your content component or content here */}
+    </div>
+  </div>
   )
 }
 
