a4a61573f29d21a0e25f6fe4f4d825d2d42e3412
AmiR <amirmsvi66@gmail.com>
2024-04-23 01:07:54 -0400

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import SignupPage from './components/SignupPage';
 import DashboardPage from './components/DashboardPage'; 
@@ -10,21 +10,6 @@ import ProtectedRoute from './components/PotectedRoute';
 
 const App: React.FC = () => {
 
-  // const isUserLoggedIn = () => {
-  //   const token = localStorage.getItem('token');
-  
-  //   if (!token) {
-  //     return false; // No token found
-  //   }
-  
-  //   try {
-  //     const decoded = jwtDecode(token); // Decode the JWT
-  //     const currentTime = Date.now() / 1000; // Get the current time in seconds
-  //     return decoded.exp !== undefined && decoded.exp > currentTime; // Check if the token is expired
-  //   } catch (error) {
-  //     return false; // If there's an error (e.g., invalid token), treat it as not logged in
-  //   }
-  // };
 
   return (
     <Router>
